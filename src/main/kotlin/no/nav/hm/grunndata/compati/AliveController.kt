f3299ade143785ae6d01d19e797a96cc0079c0e3
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-01-17 08:04:56 +0100

    added alive controller

---
+++ src/main/kotlin/no/nav/hm/grunndata/compati/AliveController.kt
@@ -0,0 +1,22 @@
+package no.nav.hm.grunndata.compati
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import no.nav.helse.rapids_rivers.KafkaRapid
+
+@Controller("/internal")
+class AliveController(private val kafkaRapid: KafkaRapid) {
+
+
+    @Get("/isAlive")
+    fun alive(): String {
+        if (kafkaRapid.isConsumerClosed()) {
+            throw Exception("Error: consumer is closed")
+        }
+        return "ALIVE"
+    }
+
+    @Get("/isReady")
+    fun ready() = "OK"
+
+}
