78dee02992defa534f5581ce8ab1d9d4445282fd
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-01-29 11:44:36 +0100

    minor adjust for frequency

--- src/main/kotlin/no/nav/hm/grunndata/compati/product/QueryBuilder.kt
+++ src/main/kotlin/no/nav/hm/grunndata/compati/product/QueryBuilder.kt
@@ -24,7 +24,7 @@ class QueryBuilder {
                           "min_term_freq": 1,
                           "max_query_terms": 25,
                           "min_doc_freq": 1,
-                          "max_doc_freq": 500
+                          "max_doc_freq": 300
                         }
                       },
                       {
